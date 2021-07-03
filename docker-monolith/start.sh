5c141fd39adf9dc37150187df7e1dec4656c5f69
Oleg Fedyakin <acid.lynx@ya.ru>
2021-07-04 01:24:06 +0300

    Fix dockerfile and mongod settings

--- docker-monolith/start.sh
+++ docker-monolith/start.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-/usr/bin/mongod --fork --logpath /var/log/mongod.log --config /etc/mongod.conf
+sleep 15
+
+/usr/bin/mongod --fork --logpath /var/log/mongod.log --config /etc/mongodb.conf
 
 source /reddit/db_config
+
+sleep 20
 
 cd /reddit && puma || exit
 
